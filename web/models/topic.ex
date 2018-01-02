922c7fd0f128e9e1adcc1027149fe28d7932c61e
Jose Alvarez <jp.alvarezl@gmail.com>
2018-01-02 22:37:58 +0100

    Corrected module name Topic

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,7 +1,13 @@
-defmodule Discuss.topic do
+defmodule Discuss.Topic do
   use Discuss.Web, :model
 
   schema "topics" do
     field :title, :string
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
   end
 end
