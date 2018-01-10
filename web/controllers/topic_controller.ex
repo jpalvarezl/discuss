1a012b20125e8766bc648a406eb119e9ca0c14df
Jose Alvarez <jp.alvarezl@gmail.com>
2018-01-10 20:31:13 +0100

    Error displaying when invalid title is provided

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,12 +2,17 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
   end
 
-  def create(conn, changeset: changeset) do
-
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert changeset do
+      {:ok, post} -> IO.inspect post
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
